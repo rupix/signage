4b87ffd63607595d3e46ccfe53a1f79a012dc63e
Geisimar <falecom@rupix.com.br>
2017-10-20 14:36:53 -0300

    remove

--- config/initializers/bugsnag.rb
+++
@@ -1,9 +0,0 @@
-Bugsnag.configure do |config|
-    config.notify_release_stages = ["production", "staging"]
-end
-
-unless Rails.env.development? || Rails.env.test?
-  Bugsnag.configure do |config|
-    config.api_key = Rails.application.secrets.bugsnag_api_key
-  end
-end
