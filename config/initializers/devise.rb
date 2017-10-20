82984dbb1d69c2e627b14881fd21a9270abbf320
Geisimar <falecom@rupix.com.br>
2017-10-20 14:57:03 -0300

    ajuste login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,9 +2,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
 
-  config.warden do |manager|
-    manager.default_strategies(scope: :user).unshift :ldap_authenticatable
-  end
+  
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
