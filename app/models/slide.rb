087f91da2ceec6097eac6c9e0357bda836e883d4
Geisimar <falecom@rupix.com.br>
2018-01-04 08:58:50 -0300

    ajuste minimagick

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -116,7 +116,7 @@ class Slide < ActiveRecord::Base
       save!
     else
       f = Screencap::Fetcher.new(Rails.application.routes.url_helpers.preview_slide_url(self))
-      file  = f.fetch output: Rails.root.join('tmp', "screenshot_of_slide_#{id}.png")
+      file  = f.fetch output: Rails.root.join('tmp', "screenshot_of_slide_#{id}.jpg")
       image = MiniMagick::Image.open(file)
       image.trim
       self.screenshot = image
